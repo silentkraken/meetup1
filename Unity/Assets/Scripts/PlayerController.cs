e250b7f569ac85a419e5d569a8696b31b7fd3ecd
Daniel Rodriguez <itsalllowercasenospaces@gmail.com>
2014-04-03 21:34:09 -0700

    Movement and initial scene

--- Unity/Assets/Scripts/PlayerController.cs
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+  public float movementSpeed = 1f;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+    Vector3 pos = transform.position;
+    if (Input.GetKeyDown(KeyCode.UpArrow)) {
+      pos.z += movementSpeed;
+    }
+    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+      pos.z -= movementSpeed;
+    }
+    transform.position = pos;
 	}
 }
