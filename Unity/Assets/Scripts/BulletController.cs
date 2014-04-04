5162bf64448b7f4067f86ea694c3cdcd888edd3d
Damon <drocco@crittercism.com>
2014-04-03 22:19:25 -0700

    Initial bullet work

---
+++ Unity/Assets/Scripts/BulletController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public float bulletSpeed;
+	public Vector3 bulletDir;
+
+	public Rect clipRect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos += bulletDir.normalized*(bulletSpeed*Time.fixedDeltaTime);
+		transform.position = pos;
+
+		//if (clipRect.Contains(Vector2(pos.x, pos.z)))
+		//{
+		//	Destroy(this);
+		//}
+	}
+}
---
+++ Unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bulletType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject.Instantiate(bulletType, transform.position, Quaternion.identity);
+		}
+	}
+}
